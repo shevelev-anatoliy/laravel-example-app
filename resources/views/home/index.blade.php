19c987493c430e9e8c964fb10069c784cdd01328
Breathless <tolik.breathless@gmail.com>
2024-03-31 13:29:43 +0700

    Создана 'view' для главной страницы

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Example App</title>
+</head>
+<body>
+ Главная
+</body>
+</html>
