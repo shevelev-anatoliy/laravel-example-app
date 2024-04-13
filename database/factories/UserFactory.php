743cadb93a1cd50f18d1bb30e58688550c569875
Breathless <tolik.breathless@gmail.com>
2024-04-13 14:44:37 +0700

    Обновлён класс `UserFactory` в соответствии с новым набором полей модели `User`

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'middle_name' => fake()->middleName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
