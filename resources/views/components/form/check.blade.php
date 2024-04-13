c0ff0e3b0e267373767cf87f905de6c2c355d449
Breathless <tolik.breathless@gmail.com>
2024-04-13 16:29:17 +0700

    Сохранение состояния чекбокса при возвращении на страницу форму

--- resources/views/components/form/check.blade.php
+++ resources/views/components/form/check.blade.php
@@ -1,7 +1,7 @@
 @props(['name' => ''])
 
 <label class="form-check flex items-start">
-    <input type="checkbox" name="{{ $name }}" value="1" {{ $attributes }} class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative top-1">
+    <input type="checkbox" name="{{ $name }}" value="1" @checked(old($name)) {{ $attributes }} class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600 relative top-1">
 
     <div class="ml-3 block text-sm leading-5 text-gray-900">
         {{ $slot }}
