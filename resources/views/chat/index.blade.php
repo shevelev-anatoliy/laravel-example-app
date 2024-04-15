0c07cd7427098b90650b00b9c1a6b8c7514a1649
shevelev <tolik.breathless@gmail.com>
2024-04-15 13:01:21 +0700

    Увеличена высота чата

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -4,7 +4,7 @@
     @auth
     <div>{{ auth()->user()->first_name }}</div>
 
-    <div class="flex h-96 antialiased text-gray-800">
+    <div class="flex h-[40rem] antialiased text-gray-800">
         <div class="flex flex-col flex-auto h-full p-6">
             <div class="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
                 <div class="flex flex-col h-full overflow-x-auto mb-4">
