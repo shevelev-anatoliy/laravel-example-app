0d0a5ff47253b2b271602dcfc782609a621ce338
Breathless <tolik.breathless@gmail.com>
2024-05-01 16:51:44 +0700

    Установлен и инициализирован слайдер Swiper

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,37 @@ const app = createApp({
         ChatMessages,
         ChatForm,
     }
-})
+});
 
-app.mount('#app')
+app.mount('#app');
+
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    modules: [Navigation, Pagination],
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,9 +1,26 @@
 <x-layouts.base>
     <x-slot:title>Главная</x-slot:title>
 
-    Главная
+    <!-- Slider main container -->
+    <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            @foreach($slides as $slide)
+            <div class="swiper-slide">
+                <img src="{{ Storage::url($slide->image) }}" alt=""/>
+            </div>
+            @endforeach
+            ...
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
 
-    @foreach($slides as $slide)
-        <img src="{{ Storage::url($slide->image) }}" alt=""/>
-    @endforeach
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
+    </div>
 </x-layouts.base>
