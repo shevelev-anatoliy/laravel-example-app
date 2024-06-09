46563c4d158b271c109c4463d0cb0f300ae8f6c5
shevelev-anatoliy <tolik.breathless@yandex.ru>
2024-06-09 12:53:07 +0700

    feat: добавлен пример переопределения рендеринга встроенного исключения Laravel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -24,5 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
             ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AccessDeniedHttpException $e) {
+            return response(trans('This action is unauthorized.'), 403);
+        });
     })->create();
