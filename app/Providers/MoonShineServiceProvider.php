70107ff6e2fb3282bfbcfec4ba5e513dc89388f8
shevelev-anatoliy <tolik.breathless@yandex.ru>
2024-06-23 11:16:43 +0700

    refactor: небольшой рефакторинг меню админки

--- app/Providers/MoonShineServiceProvider.php
+++ app/Providers/MoonShineServiceProvider.php
@@ -41,27 +41,18 @@ class MoonShineServiceProvider extends MoonShineApplicationServiceProvider
     protected function menu(): array
     {
         return [
-            MenuGroup::make(static fn() => __('moonshine::ui.resource.system'), [
-               MenuItem::make(
-                   static fn() => __('moonshine::ui.resource.admins_title'),
-                   new MoonShineUserResource()
-               ),
-               MenuItem::make(
-                   static fn() => __('moonshine::ui.resource.role_title'),
-                   new MoonShineUserRoleResource()
-               ),
-            ]),
+            MenuGroup::make(static fn() => 'moonshine::ui.resource.system', [
+               MenuItem::make('moonshine::ui.resource.admins_title', new MoonShineUserResource())
+                   ->translatable(),
+               MenuItem::make('moonshine::ui.resource.role_title', new MoonShineUserRoleResource())
+                   ->translatable(),
+            ])->translatable(),
 
-            MenuGroup::make(static fn() => 'Ресурсы', [
-                MenuItem::make(
-                    'Статьи',
-                    new PostResource()
-                )->canSee(function (MoonShineRequest $request) {
-                    return $request->user()->isHavePermission(
-                        PostResource::class,
-                        'view'
-                    );
-                }),
+            MenuGroup::make('Ресурсы', [
+                MenuItem::make('Статьи', new PostResource())
+                    ->canSee(function (MoonShineRequest $request) {
+                        return $request->user()->isHavePermission(PostResource::class, 'view');
+                    }),
 
                 MenuItem::make('Слайды', new SlideResource()),
             ]),
